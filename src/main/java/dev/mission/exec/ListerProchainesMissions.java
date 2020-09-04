f8eda07ea7cc3d921e574bfff9009626beee8876
ElodieWDJ <56838354+ElodieWDJ@users.noreply.github.com>
2020-09-04 14:32:52 +0200

    TP5.1

--- src/main/java/dev/mission/exec/ListerProchainesMissions.java
+++ src/main/java/dev/mission/exec/ListerProchainesMissions.java
@@ -2,8 +2,39 @@ package dev.mission.exec;
 
 import java.util.List;
 
-import dev.mission.entite.Mission;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
 
-public class ListerProchainesMissions {
-	List<Mission> findByLocalDate();
+import dev.mission.MissionAppApplication;
+import dev.mission.entite.Mission;
+import dev.mission.repository.MissionRepository;
+
+@Controller
+@Profile("listerNext")
+
+public class ListerProchainesMissions implements Runnable {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MissionAppApplication.class);
+
+	private MissionRepository missionRepository;
+
+	public ListerProchainesMissions(MissionRepository missionRepository) {
+		super();
+		this.missionRepository = missionRepository;
+	}
+
+	@Override
+	public void run() {
+		List<Mission> listMissionsNext = MissionRepository.listMissionsNext();
+		if (listMissionsNext.isEmpty()) {
+			LOG.warn("Pas de mission!");
+		} else {
+			for (Mission mission : listMissionsNext) {
+				LOG.warn(mission.getLibelle());
+			}
+		}
+	}
+
 }
